6f3e2fb396183ae559eb6328249c8b618a7ec860
chaitanya anand <chaitanya.anand@gmail.com>
2023-03-29 23:26:44 +0530

    desktop version done

--- src/pages/memories.js
+++ src/pages/memories.js
@@ -9,14 +9,14 @@ const memories = () => {
       </h2>
 
 
-      <div className='grid grid-cols-2 memories-grid px-5 gap-3 mb-10'>
-        <img src="/memories/1.jpg" className='col-span-2' alt="" />
-        <img src="/memories/6.jpg" className='col-span-1 object-cover h-full' alt="" />
-        <img src="/memories/2.jpg" className='col-span-1' alt="" />
-        <img src="/memories/5.jpg" className='col-span-1 row-span-2' alt="" />
+      <div className='grid grid-cols-2 memories-grid px-5 gap-3 mb-10 md:grid-cols-4'>
+        <img src="/memories/1.jpg" className='col-span-2 ' alt="" />
+        <img src="/memories/6.jpg" className='col-span-1 object-cover  md:row-span-2' alt="" />
+        <img src="/memories/2.jpg" className='col-span-1 md:row-span-2' alt="" />
+        <img src="/memories/5.jpg" className='col-span-1 row-span-2 md:object-cover ' alt="" />
         <img src="/memories/3.jpg" className='col-span-1' alt="" />
-        <img src="/memories/4.jpg" className='col-span-1' alt="" />
-        <video controls className='col-span-2'>
+        <img src="/memories/4.jpg" className='col-span-1 md:object-cover md:col-span-2' alt="" />
+        <video controls className='col-span-2 md:col-span-1'>
         <source src="/memories/7.mp4" />
       </video>
         
